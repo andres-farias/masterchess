e2118ee279ad5cb19f740d42c1fcda7597dd544d
Andres Spirits <andres.farias@gmail.com>
2020-06-09 16:29:34 -0400

    More tests for robustness.

--- src/test/java/cl/azulprofundo/masterchess/model/BoardPositionTest.java
+++ src/test/java/cl/azulprofundo/masterchess/model/BoardPositionTest.java
@@ -1,6 +1,5 @@
 package cl.azulprofundo.masterchess.model;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,12 +9,31 @@ public class BoardPositionTest {
     @Test
     public void testValid() {
         BoardPosition boardPosition = new BoardPosition(BoardColumnsEnum.A, 1);
-        Assert.assertTrue(boardPosition.isValid());
+        assertTrue(boardPosition.isValid());
+        assertFalse(boardPosition.isNullPosition());
     }
 
     @Test
     public void testInvalid() {
         BoardPosition boardPosition = new BoardPosition(null, 1);
-        Assert.assertFalse(boardPosition.isValid());
+        assertFalse(boardPosition.isValid());
+    }
+
+    @Test
+    public void testIsNull() {
+        BoardPosition nullPosition = BoardPosition.getNullPosition();
+        assertTrue(nullPosition.isNullPosition());
+    }
+
+    @Test
+    public void testEquals() {
+        BoardPosition boardPosition1 = new BoardPosition(BoardColumnsEnum.A, 1);
+        BoardPosition boardPosition2 = new BoardPosition(BoardColumnsEnum.A, 2);
+        BoardPosition boardPosition3 = new BoardPosition(BoardColumnsEnum.A, 1);
+
+        assertEquals(boardPosition1, boardPosition3);
+        assertNotEquals(boardPosition1, boardPosition2);
+        assertNotEquals(boardPosition2, boardPosition3);
+
     }
 }
